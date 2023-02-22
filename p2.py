47484abbcc8351b18d279c7fd36b25846d2f3beb
20A31A0588 <109955849+20A31A0588@users.noreply.github.com>
2023-02-22 15:04:54 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,8 @@
+class student:
+    name="mounika"
+    roll_num="20a31a0588"
+    def p_details(self):
+     print(f"name:{self.name} and roll_num:{self.roll_num}")
+ob1=student()
+ob1.p_details()           
+
